feefd4ef87023b1c078245e90c1d3e821651358b
Uday Deshpande <udaydeshpande95@gmail.com>
2023-04-30 10:20:56 +0530

    Delete macros.h

--- macros.h
+++
@@ -1,9 +0,0 @@
-/* Common utilities used in firmware */
-
-/* Helper macros */
-
-/* Get 8-byte aligned memory */
-#define ALIGN8(x) (((x)+7) & (~7))
-
-/* Get 4-byte aligned memory */
-#define ALIGN8(x) (((x)+3) & (~3))
